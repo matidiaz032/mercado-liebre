8600d9f0637f3ab224a573d1881f21d53016b75e
matidiaz032 <maty_032@hotmail.es>
2021-11-14 21:05:46 -0300

    end forms

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, './views/home.html'))
 });
 
+app.get('/register', function (req, res){
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', function (req, res){
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
 // Server
 app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}
 http://localhost:${PORT}`));
